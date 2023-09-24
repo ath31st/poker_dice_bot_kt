02a50b81b993cc35c128e68d73cc7678bfafaa41
Vladimir Olennikov <ath31st@yandex.ru>
2023-09-24 21:09:42 +0300

    init project

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,47 @@
+package org.example.botfarm
+
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.telegramError
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.logging.LogLevel
+import org.slf4j.LoggerFactory
+
+object AppKt {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    // 1. bot token
+    @JvmStatic
+    fun main(args: Array<String>) {
+        logger.info("application starting...")
+        val botToken = args[0]
+
+        val bot = bot {
+            logLevel = LogLevel.Error
+            token = botToken
+            dispatch {
+                text {
+                    val chatId = ChatId.fromId(update.message!!.chat.id)
+                    bot.sendMessage(
+                        chatId = chatId,
+                        text = message.text!!
+                    )
+                }
+                command("start") {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(update.message!!.chat.id),
+                        text = "Bot started"
+                    )
+                }
+                telegramError {
+                    println(error.getErrorMessage())
+                }
+            }
+        }
+
+        bot.startPolling()
+        logger.info("bot successfully started")
+    }
+}
