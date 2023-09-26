9c39248e8231a4b853f5664eb7275fc3cde5bd94
Vladimir Olennikov <ath31st@yandex.ru>
2023-09-26 17:28:50 +0300

    update RoundService and main cycle

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -7,17 +7,25 @@ import com.github.kotlintelegrambot.dispatcher.telegramError
 import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.ParseMode
 import com.github.kotlintelegrambot.logging.LogLevel
+import org.example.botfarm.entity.PokerRound
+import org.example.botfarm.service.PlayerService
+import org.example.botfarm.service.ResultService
+import org.example.botfarm.service.RoundService
 import org.example.botfarm.util.Command
 import org.example.botfarm.util.MessageEnum
 import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
 
 object AppKt {
     private val logger = LoggerFactory.getLogger(javaClass)
+    private val rounds: ConcurrentMap<Long, PokerRound> = ConcurrentHashMap()
 
     // 1. bot token
     @JvmStatic
     fun main(args: Array<String>) {
         DatabaseFactory.init()
+        val roundService = RoundService(PlayerService(), ResultService(), rounds)
 
         logger.info("application starting...")
         val botToken = args[0]
@@ -27,9 +35,12 @@ object AppKt {
             token = botToken
             dispatch {
                 command(Command.START.value) {
+                    val groupId = update.message!!.chat.id
+                    val playerInitiator = update.message!!.from!!.id
+                    val playerName = update.message!!.from!!.firstName //TODO firstName may be NULL
                     bot.sendMessage(
-                        chatId = ChatId.fromId(update.message!!.chat.id),
-                        text = "Started!"
+                        chatId = ChatId.fromId(groupId),
+                        text = roundService.startNewRound(groupId, playerInitiator, playerName)
                     )
                 }
                 command(Command.ROLL.value) {
--- src/main/kotlin/service/RoundService.kt
+++ src/main/kotlin/service/RoundService.kt
@@ -13,29 +13,24 @@ class RoundService(
     private val resultService: ResultService,
     private val rounds: ConcurrentMap<Long, PokerRound>
 ) {
-    suspend fun startNewRound(groupId: Long, playerInitiator: Long): String {
+    suspend fun startNewRound(groupId: Long, playerInitiator: Long, playerName: String): String {
         if (rounds.containsKey(groupId)) {
             return MessageEnum.TABLE_BUSY.value
         }
-
         val players = HashMap<Long, PlayerInRound>()
 
         val instant = Clock.System.now()
         val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
 
-        val pr = PokerRound(
+        rounds[groupId] = PokerRound(
             playerInitiator,
             isEnded = false,
             groupId,
             players,
             localDateTime,
-            0
+            actionCounter = 0
         )
-        rounds[groupId] = pr
 
-        return String().format(
-            MessageEnum.START_ROUND.value,
-            playerService.playerName(playerInitiator)
-        )
+        return MessageEnum.START_ROUND.value.format(playerName)
     }
 }
