b7db7f6d58b369582491b51feb6eeccb4dfc09f3
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-03 17:57:03 +0300

    added KDoc for Result, RoundResult

--- src/main/kotlin/org/example/botfarm/entity/Result.kt
+++ src/main/kotlin/org/example/botfarm/entity/Result.kt
@@ -4,6 +4,14 @@ import java.time.LocalDateTime
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 
+/**
+ * Represents a game result.
+ *
+ * @property resultId   The unique identifier of the game result.
+ * @property player     The player associated with the result.
+ * @property roundTime  The date and time when the result was recorded.
+ * @property groupId    The identifier of the group to which the result belongs.
+ */
 class Result(
     val resultId: Long,
     val player: Player,
@@ -11,6 +19,9 @@ class Result(
     val groupId: Long,
 )
 
+/**
+ * Represents the database table for storing game results.
+ */
 object Results : Table("results") {
     val resultId = long("result_id").autoIncrement()
     val playerId = long("player_id") references Players.id
--- src/main/kotlin/org/example/botfarm/entity/RoundResult.kt
+++ src/main/kotlin/org/example/botfarm/entity/RoundResult.kt
@@ -2,6 +2,13 @@ package org.example.botfarm.entity
 
 import org.example.botfarm.util.Combination
 
+/**
+ * Represents the result of a round in the game.
+ *
+ * @property score       The score achieved in the round.
+ * @property combination The combination of dice values that led to this result.
+ * @property priority    The priority of this result, used for ranking in the game.
+ */
 class RoundResult(
     val score: Int,
     val combination: Combination,
