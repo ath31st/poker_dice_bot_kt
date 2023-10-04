6cf20e90a861376cb8854186b9ff1083139012de
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-04 09:09:04 +0300

    added KDoc for PokerDiceScheduler, MessageService

--- src/main/kotlin/org/example/botfarm/scheduler/PokerDiceScheduler.kt
+++ src/main/kotlin/org/example/botfarm/scheduler/PokerDiceScheduler.kt
@@ -4,9 +4,24 @@ import java.time.LocalDateTime
 import java.util.concurrent.ConcurrentMap
 import org.example.botfarm.entity.PokerRound
 
+/**
+ * The `PokerDiceScheduler` object manages the scheduling and finalization of poker rounds.
+ * It provides a method to determine which rounds should be finalized based on their start time
+ * and a predefined duration.
+ */
 object PokerDiceScheduler {
+    // The duration (in minutes) after which a poker round should be finalized if not already done.
     private const val DURATION = 5L
 
+    /**
+     * Finalizes poker rounds that have exceeded the specified duration since their start time.
+     *
+     * @param rounds A concurrent map containing active poker rounds, where the key is the group ID
+     *               and the value is a [PokerRound] object representing the round.
+     *
+     * @return A list of pairs, where each pair contains the group ID of a finalized round and a list
+     *         of player names who have automatically passed their turn during that round.
+     */
     fun finalizeRounds(
         rounds: ConcurrentMap<Long, PokerRound>,
     ): List<Pair<Long, List<String>>> {
--- src/main/kotlin/org/example/botfarm/service/MessageService.kt
+++ src/main/kotlin/org/example/botfarm/service/MessageService.kt
@@ -9,7 +9,18 @@ import org.example.botfarm.util.MessageEnum
 import org.example.botfarm.util.RandomPhrase
 import org.example.botfarm.util.StringUtil
 
+/**
+ * The `MessageService` class provides methods for preparing text messages related to a game or round.
+ */
 class MessageService {
+
+    /**
+     * Prepares a text message after starting a round based on the startRoundStatus.
+     *
+     * @param startRoundStatus The status code indicating the result of starting the round.
+     * @param playerName The name of the player.
+     * @return A formatted text message.
+     */
     fun prepareTextAfterStartingRound(startRoundStatus: Int, playerName: String): String {
         return when (startRoundStatus) {
             1 -> {
@@ -26,6 +37,13 @@ class MessageService {
         }
     }
 
+    /**
+     * Prepares a text message after rolling dices, including the player's name and the dice results.
+     *
+     * @param rollDices An array containing the rolled dice values.
+     * @param playerName The name of the player.
+     * @return A formatted text message.
+     */
     fun prepareTextAfterRollDices(rollDices: IntArray?, playerName: String): String {
         val text = if (rollDices == null || rollDices.isEmpty()) {
             ""
@@ -36,6 +54,14 @@ class MessageService {
         return text
     }
 
+    /**
+     * Prepares a text message after rerolling dices, including the player's name and the new and original dice results.
+     *
+     * @param firstRoll An array containing the original dice values.
+     * @param reroll An array containing the new dice values after rerolling.
+     * @param playerName The name of the player.
+     * @return A formatted text message.
+     */
     fun prepareTextAfterRerollDices(
         firstRoll: IntArray,
         reroll: IntArray,
@@ -53,14 +79,33 @@ class MessageService {
         return text
     }
 
+    /**
+     * Prepares a text message after a player decides to pass their turn.
+     *
+     * @param playerName The name of the player.
+     * @return A formatted text message.
+     */
     fun prepareTextAfterPass(playerName: String): String {
         return RandomPhrase.getPassPhrase().format(playerName)
     }
 
+    /**
+     * Prepares a text message after finishing a round, including the player's name.
+     *
+     * @param playerName The name of the player.
+     * @return A formatted text message.
+     */
     fun prepareTextAfterFinishRound(playerName: String): String {
         return MessageEnum.FINISH_ROUND.value.format(playerName)
     }
 
+    /**
+     * Prepares a text message displaying the results of a round.
+     *
+     * @param result A map containing round results with player IDs as keys and RoundResult objects as values.
+     * @param players A map containing player information with player IDs as keys and PlayerInRound objects as values.
+     * @return A formatted text message displaying round results.
+     */
     fun prepareResultText(
         result: Map<Long, RoundResult>,
         players: Map<Long, PlayerInRound>,
@@ -74,6 +119,13 @@ class MessageService {
                     }.collect(Collectors.joining("\n")) + "```"
     }
 
+    /**
+     * Prepares a text message displaying the leaderboard for the game group.
+     *
+     * @param roundsCount The total number of rounds played.
+     * @param leaders A map containing player names as keys and their scores as values.
+     * @return A formatted text message displaying the leaderboard.
+     */
     fun prepareLeaderBoardText(roundsCount: Int, leaders: Map<String, Int>): String {
         return "=====================\n```\nЗа прошедшую неделю сыграно: $roundsCount раунда(ов).\nТаблица лидеров этой группы (Топ 5):\n" +
                 leaders.entries
@@ -84,6 +136,12 @@ class MessageService {
                     .collect(Collectors.joining("\n")) + "```"
     }
 
+    /**
+     * Prepares a text message for auto-passing a player's turn.
+     *
+     * @param playerName The name of the player.
+     * @return A formatted text message for auto-passing.
+     */
     fun prepareAutoPassText(playerName: String): String {
         return RandomPhrase.getAutoPassPhrase().format(playerName)
     }
